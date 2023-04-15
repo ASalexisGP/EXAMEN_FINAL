4ca5e11b1a8b564ce696d94276f7d93aaf46f35b
alexi <you@example.comexitsalirhomea git config --global user.email you@example.com>
2023-04-15 00:37:24 -0500

    Empezando con git hub

---
+++ src/main/java/com/onpe/FinalOnpeGpApplication.java
@@ -0,0 +1,13 @@
+package com.onpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalOnpeGpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalOnpeGpApplication.class, args);
+	}
+
+}
